5cf4f9d2855ae3f9ede94c17a03308aef9f31ae2
kauan almeida <kauansouzapoielli@gmail.com>
2024-05-08 14:35:59 -0300

    pequenas alteracoes

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,61 @@
-<script setup>
-import { reactive } from "vue";
-
-    const Vue = reactive({
-    el: '#app',
-    data: {
-        numero1: 0,
-        numero2: 0,
-        operador: 'add',
-        resultado: 0
-    },
-    methods: {
-        calculate: function () {
-            switch (this.operador) {
-                case 'add':
-                    this.resultado = parseFloat(this.numero1) + parseFloat(this.numero2);
-                    break;
-                case 'subtract':
-                    this.resultado = parseFloat(this.numero1) - parseFloat(this.numero2);
-                    break;
-                case 'multiply':
-                    this.resultado = parseFloat(this.numero1) * parseFloat(this.numero2);
-                    break;
-                case 'divide':
-                    if (parseFloat(this.numero2) !== 0) {
-                        this.resultado = parseFloat(this.numero1) / parseFloat(this.numero2);
-                    } else {
-                        this.resultado = 'Não é possível dividir por zero';
-                    }
-                    break;
-                default:
-                    this.resultado = 0;
-            }
-        }
-    }
-})
-</script>
-
 <template>
-  <div id="app">
-        <input type="number" v-model.number="numero1" @input="calculate">
-        <select v-model="operador" @change="calculate">
-            <option value="add">+</option>
-            <option value="subtract">-</option>
-            <option value="multiply">*</option>
-            <option value="divide">/</option>
-        </select>
-        <input type="number" v-model.number="numero2" @input="calculate">
-        <p>resultado: {{ resultado }}</p>
-    </div>
+  <div class="calculadora">
+    <input type="number" v-model="numero1" @input="calcularResultado">
+    <select v-model="operacao" @change="calcularResultado">
+      <option value="adição+">+</option>
+      <option value="subtração-">-</option>
+      <option value="multiplicação*">*</option>
+      <option value="divisão/">/</option>
+    </select>
+    <input type="number" v-model="numero2" @input="calcularResultado">
+    <div>Seu resultado é: {{ resultado }} !</div>
+  </div>
 </template>
 
-<style scoped>
+<script>
+export default {
+  data() {
+    return {
+      numero1: 0,
+      numero2: 0,
+      operacao: '+',
+      resultado: 0
+    };
+  },
+  methods: {
+    calcularResultado() {
+      switch (this.operacao) {
+        case 'adição+':
+          this.resultado = parseFloat(this.numero1) + parseFloat(this.numero2);
+          break;
+        case 'subtração-':
+          this.resultado = parseFloat(this.numero1) - parseFloat(this.numero2);
+          break;
+        case 'multiplicação*':
+          this.resultado = parseFloat(this.numero1) * parseFloat(this.numero2);
+          break;
+        case 'divisão/':
+          this.resultado = parseFloat(this.numero1) / parseFloat(this.numero2);
+          break;
+        default:
+          this.resultado = 0;
+      }
+    }
+  }
+};
+</script>
 
+<style>
+.calculadora {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+input, select {
+  margin: 5px solid green;
+}
+
+body {
+  background: linear-gradient(to right, #ccc, rgb(130, 235, 235));
+}
 </style>
